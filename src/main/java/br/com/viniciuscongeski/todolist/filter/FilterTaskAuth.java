6cbb140bd885d30e2505f1e88f739e29950b8839
Vinicius Congeski <viniciuscongelo@gmail.com>
2023-10-14 16:34:10 -0300

    Criação do Utils e melhoria no código

--- src/main/java/br/com/viniciuscongeski/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/viniciuscongeski/todolist/filter/FilterTaskAuth.java
@@ -26,7 +26,7 @@ public class FilterTaskAuth extends OncePerRequestFilter {
 
         var servletPath = request.getServletPath();
 
-        if (servletPath.equals("/tasks/")) {
+        if (servletPath.startsWith("/tasks/")) {
             // Pegar a autenticação (usuario e senha)
             var authorization = request.getHeader("Authorization");
             var authEncoded = authorization.substring("Basic".length()).trim();
@@ -49,6 +49,7 @@ public class FilterTaskAuth extends OncePerRequestFilter {
                 // Validar Senha
                 var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
                 if (passwordVerify.verified) {
+                    request.setAttribute("idUser", user.getId());
                     filterChain.doFilter(request, response);
                 } else {
                     response.sendError(401);
